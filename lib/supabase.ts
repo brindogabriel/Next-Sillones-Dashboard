892502d7f57cfc9804974b7bbc9b22e0e1efaae3
Gabriel Brindo <gabrielbrindo18@gmail.com>
2025-04-25 22:16:59 -0300

    fix: añadir validación para la variable de entorno NEXT_PUBLIC_SUPABASE_ANON_KEY en supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,6 +3,10 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = 'https://pbvlntfpulqpjizycftt.supabase.co'
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
+if (!supabaseKey) {
+  throw new Error("Falta la variable de entorno NEXT_PUBLIC_SUPABASE_ANON_KEY");
+}
+
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: { persistSession: false },
 });
